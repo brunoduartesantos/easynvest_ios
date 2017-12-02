e26d9af6987b9cbc3d9e59868eff943017710c58
Bruno Pereira <brunopereiras@icloud.com>
2017-12-01 22:39:56 -0200

    Removido o storyboard, criado o view controller inicial do projeto, organizado as pastas do projeto.

---
+++ Easynvest iOS/SimulateViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SimulateViewController.swift
+//  Easynvest iOS
+//
+//  Created by Bruno Pereira dos Santos on 01/12/17.
+//
+
+import UIKit
+
+class SimulateViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Easynvest iOS/Support Files/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Easynvest iOS
+//
+//  Created by Bruno Pereira dos Santos on 01/12/2017.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        setupInitialViewController()
+        return true
+    }
+
+    /// Initialize the main navigation controller and the first view controller
+    func setupInitialViewController() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let simulateViewController = SimulateViewController()
+        let navigationController = UINavigationController(rootViewController: simulateViewController)
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+    }
+}
